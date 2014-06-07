caa76a0eafacfbf81796c17ec90528733590abd4
yoshikizh <177365340@qq.com>
2014-06-08 03:47:55 +0800

    修正发帖字数限制问题

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   validates :name,:content,  presence: true
-  validates_length_of :name, :in => 6..32, message: "字数必须在 6 - 32 之间"
+  validates_length_of :name, :in => 6..64, message: "字数必须在 6 - 32 之间"
   belongs_to :user
   belongs_to :node
   has_many :replies, class_name: TopicReply, foreign_key: "topic_id"
