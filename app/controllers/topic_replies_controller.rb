2a9a2012281c2d4cf989b65efc9a13843c6f64bf
yoshikizh <177365340@qq.com>
2014-06-08 17:49:37 +0800

    帖子列表样式 和 新会员

--- app/controllers/topic_replies_controller.rb
+++ app/controllers/topic_replies_controller.rb
@@ -2,6 +2,7 @@ class TopicRepliesController < InheritedResources::Base
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
   def create
     @reply = TopicReply.new params.require(:topic_reply).permit(:content,:topic_id,:user_id)
+    @reply.topic.update_attributes :updated_at => Time.now
     current_user.add_point(3)
     if @reply.save
       redirect_to node_topic_path(params[:node_id],params[:topic_id]),notice: "感谢您的回复 ~ 积分 + 3"
